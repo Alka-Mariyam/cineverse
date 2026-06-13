0ed888904a30cd9e59c9d0be572eff2d3481601a
ALKA MARIYAM <alkamariyam@ALKAs-MacBook-Air.local>
2026-06-13 19:59:33 +0530

    Fix AttributeError in verify_payment QR generation

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -149,7 +149,7 @@ class BookingViewSet(viewsets.ModelViewSet):
                     from django.core.files.base import ContentFile
                     
                     seats_str = ", ".join([str(bs.seat.row) + str(bs.seat.number) for bs in booking.booked_seats.all()])
-                    qr_data = f"CINEVERSE TICKET\n------------------\nBooking ID: {booking.id}\nMovie: {booking.show.movie_title}\nTheatre: {booking.show.screen.theatre.name}\nScreen: {booking.show.screen.name}\nDate: {booking.show.date}\nTime: {booking.show.start_time}\nSeats: {seats_str}\nUser: {booking.user.username}"
+                    qr_data = f"CINEVERSE TICKET\n------------------\nBooking ID: {booking.id}\nMovie: {booking.show.movie.title}\nTheatre: {booking.show.screen.theatre.name}\nScreen: {booking.show.screen.name}\nDate: {booking.show.date}\nTime: {booking.show.start_time}\nSeats: {seats_str}\nUser: {booking.user.username}"
                     
                     img = qrcode.make(qr_data)
                     buffer = BytesIO()
